ed59703663b344e756d3db6b05ba9739a4fb4781
Frederik Petersen <fred.p@live.dk>
2023-09-21 13:27:46 +0200

    Exercise 4.2

--- Assignment04/Fun/Absyn.fs
+++ Assignment04/Fun/Absyn.fs
@@ -2,12 +2,12 @@
 
 module Absyn
 
-type expr = 
+type expr =
   | CstI of int
   | CstB of bool
   | Var of string
   | Let of string * expr * expr
   | Prim of string * expr * expr
   | If of expr * expr * expr
-  | Letfun of string * string * expr * expr    (* (f, x, fBody, letBody) *)
-  | Call of expr * expr
+  | Letfun of string * string list * expr * expr    (* CHANGED FOR ASSIGNMENT 4.3 *)
+  | Call of expr * expr list (* CHANGED FOR ASSIGNMENT 4.3 *)
